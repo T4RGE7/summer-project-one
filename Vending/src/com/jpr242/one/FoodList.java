cc49f152bf7d8b65d92dde0175cb26d800c81f46
T4RGE7 <t4rge7@gmail.com>
2013-07-02 22:19:11 -0400

    ten twenty

--- Vending/src/com/jpr242/one/FoodList.java
+++ Vending/src/com/jpr242/one/FoodList.java
@@ -1,15 +1,59 @@
 package com.jpr242.one;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class FoodList {
+public class FoodList implements Serializable{
 	private ArrayList<String[]> snacks;
 	private ArrayList<String[]> drinks;
 	
 	public FoodList() {
 		Scanner reader = null;
 		
+		snacks = new ArrayList<String[]>();
+		drinks = new ArrayList<String[]>();
 		
+		try {
+			reader = new Scanner(new File("snackList.txt"));
+			
+			while (reader.hasNextLine()) {
+				snacks.add(reader.nextLine().split(","));
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Snack List Not Found.");
+		}
+		reader.close();
+		
+		try {
+			reader = new Scanner(new File("drinkList.txt"));
+			
+			while (reader.hasNextLine()) {
+				drinks.add(reader.nextLine().split(","));
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Drink List Not Found.");
+		}
+		reader.close();
 	}
+
+	public ArrayList<String[]> getSnacks() {
+		return snacks;
+	}
+
+	public void setSnacks(ArrayList<String[]> snacks) {
+		this.snacks = snacks;
+	}
+
+	public ArrayList<String[]> getDrinks() {
+		return drinks;
+	}
+
+	public void setDrinks(ArrayList<String[]> drinks) {
+		this.drinks = drinks;
+	}
+	
+	
 }
